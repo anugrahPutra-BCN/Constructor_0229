27964b1ab463862f2892a77f7e4ee32f4539c049
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-17 16:59:33 +0700

    membuat kelas mahasiswa

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -3,3 +3,15 @@
 using namespace std;
 
 
+class mahasiswa{
+private:
+     int Nim;
+     string Nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
