052c1dee2f552523ac178a6630a1380377e387e7
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-17 16:44:36 +0700

    menambahkan construktor dan destruktor

--- dustruktor.cpp
+++ dustruktor.cpp
@@ -11,3 +11,17 @@ public:
      void cetakData();
      void isiData();
 };
+// Definisi memberr function
+angka::angka(int i) { // construktor
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){ // destruktor
+    cout<<endl;
+    cetakData();
+    delete[]arr;
+    cout<<"Alamat Array Sudah Dilepaskan"<<endl;
+}
+
