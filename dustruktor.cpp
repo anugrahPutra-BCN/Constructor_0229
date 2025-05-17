8bad324800feebee78d59eb8c92b99b5dd28bed8
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-17 16:41:42 +0700

    membuat kelas angka

--- dustruktor.cpp
+++ dustruktor.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class angka{
+private:
+     int *arr;
+     int panjang;
+public:
+     angka(int); // Construktor
+     ~angka();
+     void cetakData();
+     void isiData();
+};
